6ed4eeb70dbba47e1f857cdecc56e88e24d538ec
Javier Delgado <javiercambranis23@gmail.com>
2023-02-15 09:29:25 -0500

    add socket.io

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
-import httpServer from 'http';
+import {createServer} from 'http';
 import { Server } from 'socket.io';
 
 const app = express();
-const server = httpServer.createServer(app);
+const server = createServer();
 
+const io = new Server();
+io.attach(server);
 
 app.use('/', (req, res) => {
     return res.json({status: 'Ok'})
+});
+
+io.on("connection", (socket) => {
+    console.log('Conectado...');
 })
 
 const PORT = process.env.PORT || 3000;
