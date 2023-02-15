d18cc4496a6c5d4b398ea01fcffb264d83f153cb
Javier Delgado <javiercambranis23@gmail.com>
2023-02-15 10:57:49 -0500

    prueba 2

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer();
-app.use(cors({}))
-const io = new Server();
-io.attach(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
+app.use(cors())
+const io = new Server(server, {
+    cors: {
+        origin: "https://127.0.0.1:5173",
+        credentials: true
+      }
 });
+// io.attach(server, {
+//     cors: {
+//         origin: "https://my-frontend.com",
+//         credentials: true
+//       }
+    // handlePreflightRequest: (req, res) => {
+    //     const headers = {
+    //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    //         "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+    //         "Access-Control-Allow-Credentials": true
+    //     };
+    //     res.writeHead(200, headers);
+    //     res.end();
+    // }
+// });
 
 app.use('/', (req, res) => {
     return res.json({ status: 'Ok' })
