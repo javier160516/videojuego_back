69740cea0d086697e7c7fb66948bd00939b0fc04
Javier Delgado <javiercambranis23@gmail.com>
2023-03-08 13:36:08 -0500

    .

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getProblemas, registrarPregunta, guardarImagen } from '../controllers/gameController.js';
+import { registrarPregunta, guardarImagen } from '../controllers/gameController.js';
 import upload from '../middleware/uploadImage.js';
 
 const router = express.Router();
 
-router.get('/panel', getProblemas);
+// router.get('/panel', getProblemas);
 router.post('/registrar-ejercicio', registrarPregunta);
 router.post('/guardarImagen-ejercicio/:id', upload.array('options'), guardarImagen);
 
