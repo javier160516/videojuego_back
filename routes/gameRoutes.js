4c76415a662725db15d3ca64f242953ea60f9f13
Javier Delgado <javiercambranis23@gmail.com>
2023-03-08 14:02:56 -0500

    proeba 2.02 pm

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import upload from '../middleware/uploadImage.js';
-import { registrarPregunta, guardarImagen } from '../controllers/gameController.js';
+import { registrarPregunta, guardarImagen, getProblemas } from '../controllers/gameController.js';
 
 const router = express.Router();
 
-// router.get('/panel', getProblemas);
+router.get('/panel', getProblemas);
 router.post('/registrar-ejercicio', registrarPregunta);
 router.post('/guardarImagen-ejercicio/:id', upload.array('options'), guardarImagen);
 
